488ae36dabe8f085200d1d173e1f58c00286834b
Dinuka <dinukanh@gmail.com>
2024-04-14 17:45:17 +1000

    eventListener added to each drum class

---
+++ index.js
@@ -0,0 +1,11 @@
+const numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got clicked");
+    });
+
+}
+
+
+
